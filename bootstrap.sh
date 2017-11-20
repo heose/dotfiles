1d88ad613461c9a6872a12ab95eb1c2a26030b2e
heose <goddnrl@gmail.com>
2017-11-20 14:22:57 +0900

    zshrc 추가 및 bootstrap설정 추가

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,5 +20,13 @@ git clone https://github.com/dracula/zsh.git $HOME/.dracula-theme/zsh
 git clone https://github.com/dracula/iterm.git $HOME/.dracula-theme/iterm
 git clone https://github.com/dracula/jetbrains.git $HOME/.dracula-theme/jetbrains
 
+#zsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ln -s $HOME/.dracula-theme/zsh/dracula.zsh-theme $HOME/.oh-my-zsh/themes/dracula.zsh-theme
+[ ! -f $HOME/.zshrc ] && ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+source $HOME/.zshrc
+
+#emacs
 mkdir $HOME/.emacs.d
 ln -s $HOME/dotfiles/.emacs.d/init.el $HOME/.emacs.d/init.el
