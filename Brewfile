911da9261d13c25eea43177421a17c1b12e539cc
heose <goddnrl@gmail.com>
2017-11-20 11:38:03 +0900

    설정 추가

---
+++ Brewfile
@@ -0,0 +1,28 @@
+# dotfiles/Brewfile
+
+cask_args appdir: '/Applications'
+
+#Taps
+tap "homebrew/core"
+tap "homebrew/bundle"
+tap "caskroom/cask"
+
+#Binaries/Libraries
+brew "python3"
+brew "postgresql"
+brew "the_silver_searcher"
+
+#Applications
+cask "1password"
+cask "alfred"
+cask "bettertouchtool"
+cask "emacs"
+cask "evernote"
+cask "firefox"
+cask "forklift"
+cask "google-chrome"
+cask "iterm2"
+cask "jetbrains-toolbox"
+cask "karabiner-elements"
+cask "telegram"
+cask "transmission"
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 
+if test ! $(which brew); then
+    /usr/bin/ruby -e "$(curl -fsSLhttps://raw.githubusercontent.com/Homebrew/install/master/install)”
+fi
+
+brew update
+brew tap homebrew/bundle
+brew bundle --file=$HOME/dotfiles/Brewfile
+brew cleanup
+brew cask cleanup
+
+# git global config setting
+git config --global user.name heose
+git config --global user.email goddnrl@gmail.com
+
+# Download dracula theme
+mkdir $HOME/.dracula-theme 
+cd $HOME/.dracula-theme
+git clone git clone https://github.com/dracula/zsh.git
+git clone https://github.com/dracula/iterm.git
+git clone https://github.com/dracula/jetbrains.git
+
 ln -s $HOME/dotfiles/.emacs.d/init.el $HOME/.emacs.d/init.el
